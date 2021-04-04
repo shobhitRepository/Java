d9b241d5fca2e5aea3e70d1cd04fa10913e2a442
Shobhit Gupta <shobhgupta123@gmail.com>
2021-04-04 21:40:46 +0530

    Initial project setup no dependency added

---
+++ springboot-explore/src/main/java/com/shobhit/learntocode/springbootexplore/SpringbootExploreApplication.java
@@ -0,0 +1,13 @@
+package com.shobhit.learntocode.springbootexplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExploreApplication.class, args);
+	}
+
+}
---
+++ springboot-explore/src/test/java/com/shobhit/learntocode/springbootexplore/SpringbootExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shobhit.learntocode.springbootexplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
