de403c6e12db8a872e3919480217c0939553b3da
Rachitgupta-123 <shobhgupta123@gmail.com>
2021-04-04 23:39:29 +0530

    Spring web dependency and port added

--- springboot-explore/src/main/java/com/shobhit/learntocode/springbootexplore/SpringbootExploreApplication.java
+++ springboot-explore/src/main/java/com/shobhit/learntocode/springbootexplore/SpringbootExploreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootExploreApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringbootExploreApplication.class, args);
 	}
 
