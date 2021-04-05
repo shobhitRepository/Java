bc07de3ea5ece716a3d9d5802136b443db2548ee
Rachitgupta-123 <shobhgupta123@gmail.com>
2021-04-05 21:57:03 +0530

    Logging added slf4j

--- springboot-explore/src/main/java/com/shobhit/learntocode/springbootexplore/controller/CustomerController.java
+++ springboot-explore/src/main/java/com/shobhit/learntocode/springbootexplore/controller/CustomerController.java
@@ -16,8 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shobhit.learntocode.springbootexplore.entity.Customer;
 import com.shobhit.learntocode.springbootexplore.service.CustomerService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/customers")
+@Slf4j
 public class CustomerController {
 	
 	@Autowired
@@ -25,22 +28,27 @@ public class CustomerController {
 	
 	@GetMapping("/")
 	public List<Customer> findAll(){
+		log.info("find all customers");
 		return customerService.findAll();
 	}
 	@GetMapping("/{id}")
 	public Optional<Customer> findById(@PathVariable("id") Long id){
+		log.info("find by id customers");
 		return customerService.findById(id);
 	}
 	@PostMapping("/save")
 	public Customer saveCustomer(@RequestBody Customer customer) {
+		log.info("save all customers");
 		return customerService.saveCustomer(customer);
 	}
 	@PutMapping("/update")
 	public Customer updateCustomer(@RequestBody Customer customer) {
+		log.info("update by id customers");
 		return customerService.updateCustomer(customer);
 	}
 	@DeleteMapping("/delete/{id}")
-	public String updateCustomer(@PathVariable("id") Long id) {
+	public String deleteCustomer(@PathVariable("id") Long id) {
+		log.info("Delete by id customers");
 		customerService.DeleteCustomer(id);
 		return "deleted id "+ id;
 	}
